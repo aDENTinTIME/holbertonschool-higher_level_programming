7757ff6df20ef03bcacb4f829b0f0e1c4088626c
adentintime <arixbizz@gmail.com>
2018-04-13 06:59:56 +0000

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,14 @@
 from sys import argv
 
 
-argc = len(argv)
+def main():
+    argc = len(argv)
 
-if argc == 1:
-    print("0 arguments.")
-    exit()
-
-print(argc - 1, "arguments:")
-for i in range(1, argc):
-    print(argv[i])
-
+    if argc == 1:
+        print("0 arguments.")
+        exit()
+    print(argc - 1, "arguments:")
+    for i in range(1, argc):
+        print(argv[i])
+if __name__ == "__main__":
+    main()
