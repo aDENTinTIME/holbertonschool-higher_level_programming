03f4ffae2f1720e433fa66ed9215aeb03a6634d5
adentintime <arixbizz@gmail.com>
2018-04-13 07:08:43 +0000

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,10 @@ def main():
     if argc == 1:
         print("0 arguments.")
         exit()
-    print(argc - 1, "arguments:")
+    if argc == 2:
+        print("1 argument:")
+    else:
+        print(argc - 1, "arguments:")
     for i in range(1, argc):
         print("{:d}:".format(i), argv[i])
 if __name__ == "__main__":
