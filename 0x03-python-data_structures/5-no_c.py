d10e3a8a5ed5e9c90a8ab33b3d9ddfd04fecd1d3
adentintime <arixbizz@gmail.com>
2018-04-18 05:12:39 +0000

    5-no_c.py use string concat method

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_list = list(my_string)
-    i = 0
-    for c in range(len(new_list)):
-        if my_string[c] != ord('c') or my_string[c] != ord('C'):
-            new_list[i] = new_list[c]
-            i += 1
-        else:
-            print(c)
-    new_string = ''.join(new_list)
+    if my_string is None:
+        return
+    new_string = ""
+    for c in range(len(my_string)):
+        if my_string[c] != 'c' and my_string[c] != 'C':
+            new_string = new_string + my_string[c]
     return new_string
