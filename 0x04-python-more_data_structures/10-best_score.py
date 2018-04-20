96d622b899fd8ed10acc46fb8225828434c662da
adentintime <arixbizz@gmail.com>
2018-04-20 07:04:03 +0000

    11wip

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    point = ""
-    my_list = list(a_dictionary.keys())
+    my_list = list(a_dictionary.keys)
     for i in my_list:
-        if a_dictionary[i] > point[i]:
-            point = i
-    return point
+        
+    return max(list(a_dictionary.keys))
