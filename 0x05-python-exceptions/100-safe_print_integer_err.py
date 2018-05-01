dfe7ac0094655ef8ffb8a56ddb71a5eab0fd205e
adentintime <arixbizz@gmail.com>
2018-05-01 06:46:34 +0000

    fix 100-safe_print_integer_err.py removed exception raise and spaces in error message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,12 +4,10 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        if not isinstance(value, int):
-            raise ValueError
         print("{:d}".format(value))
         return True
 
     except ValueError:
         print("Exception: Unknown format code 'd'\
-                 for object of type 'str'", file=sys.stderr)
+ for object of type 'str'", file=sys.stderr)
         return False
