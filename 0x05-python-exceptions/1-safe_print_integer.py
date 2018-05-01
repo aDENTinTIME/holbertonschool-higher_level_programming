a68732d2195839e42f2db85c6b891759785f315a
adentintime <arixbizz@gmail.com>
2018-05-01 05:09:37 +0000

    add isinstance protection to 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
+        if not isinstance(value, int):
+            raise ValueError
         print("{:d}".format(value))
         return True
 
