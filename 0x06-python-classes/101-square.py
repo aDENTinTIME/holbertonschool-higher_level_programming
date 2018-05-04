6962caa55d705c7dcdd39140fbc877a4bb9159db
adentintime <arixbizz@gmail.com>
2018-05-04 22:06:09 +0000

    Create 101-square.py

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+def errorCheck(size):
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+
+def errorCheckTuple(value):
+    if not isinstance(value, tuple):
+        raise TypeError("position must be a tuple of 2 positive integers")
+    if len(value) != 2:
+        raise TypeError("position must be a tuple of 2 positive integers")
+    if not isinstance(value[0], int) or not isinstance(value[1], int):
+        raise TypeError("position must be a tuple of 2 positive integers")
+    if value[0] < 0 or value[1] < 0:
+        raise TypeError("position must be a tuple of 2 positive integers")
+
+
+class Square:
+
+    def __init__(self, size=0, position=(0, 0)):
+        errorCheck(size)
+        errorCheckTuple(position)
+        self.__size = size
+        self._position = position
+
+    def __str__(self):
+        if not self.__size:
+            print()
+            return
+        for y in range(self._position[1]):
+            print()
+        for height in range(self.__size):
+            for x in range(self._position[0]):
+                print(" ", end="")
+            for width in range(self.__size):
+                print("#", end="")
+            print()
+        return ""
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        errorCheck(value)
+        self.__size = value
+
+    @property
+    def position(self):
+        return self._position
+
+    @position.setter
+    def position(self, value):
+        errorCheckTuple(value)
+        self._position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if not self.__size:
+            print()
+            return
+        for y in range(self._position[1]):
+            print()
+        for height in range(self.__size):
+            for x in range(self._position[0]):
+                print(" ", end="")
+            for width in range(self.__size):
+                print("#", end="")
+            print()
--- 0x06-python-classes/main.py
+++ 0x06-python-classes/main.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-Square = __import__('6-square').Square
+Square = __import__('101-square').Square
 
-my_square = Square(0, (10, 3))
-my_square.my_print()
+my_square = Square(5, (0, 0))
+print(my_square)
+
+print("--")
+
+my_square = Square(5, (4, 1))
+print(my_square)
