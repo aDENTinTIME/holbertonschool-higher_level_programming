79796a180f6f5dadbbd753ea675f3616c95eaaae
adentintime <arixbizz@gmail.com>
2018-05-09 06:46:40 +0000

    Create advanced, work in progress

---
+++ 0x09-python-everything_is_object/one_hundred_notes.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""it would be great if i had a way of accessing the iterator `i` in the
+calling program, i could use that as my multiplier
+global i"""
+def magic_string():
+    return "Holberton" + ", Holberton" * 0
