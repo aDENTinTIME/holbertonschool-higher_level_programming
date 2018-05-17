5752da015cebd615de76914cf9e53ee2ffe854e2
adentintime <arixbizz@gmail.com>
2018-05-17 06:04:54 +0000

    Create 10-12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) == list:
+            dic = {}
+            for x in attrs:
+                if self.__dict__.__contains__(x):
+                    dic[x] = self.__dict__[x]
+            return dic
+        return self.__dict__
