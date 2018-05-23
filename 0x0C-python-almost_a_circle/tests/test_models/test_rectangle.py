1f093ea9fdf3a9c6af31b5eaca7099b3ba6e4321
adentintime <arixbizz@gmail.com>
2018-05-23 02:39:24 +0000

    Create test_rectangle.py

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Unittest for Rectangle
+"""
+
+
+import unittest
+from models.rectangle import Rectangle
+
+class TestRectangleClass(unittest.TestCase):
+    """Checks for correct output during many edge cases.
+    """
+
+    def test_1_simple(self):
+        r1 = Rectangle(1, 2)
+        r2 = Rectangle(3, 4)
+        r3 = Rectangle(5, 6, 0, 0, 44)
+        r4 = Rectangle(7, 8)
+        self.assertEqual(r1.id, 5)
+        self.assertEqual(r2.id, 6)
+        self.assertEqual(r3.id, 44)
+        self.assertEqual(r4.id, 7)
+
+    def test_2_adv(self):
+        r5 = Rectangle(3, 3, 3, 3, "string ID")
+        r6 = Rectangle(3, 3, 3, 3, (1, 2))
+        r7 = Rectangle(3, 3, 3, 3, None)
+        self.assertEqual(r5.id, "string ID")
+        self.assertEqual(r6.id, (1, 2))
+        self.assertEqual(r7.id, 8)
+
+
+if __name__ == "__main__":
+    unittest.main()
