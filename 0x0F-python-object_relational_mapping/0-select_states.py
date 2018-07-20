fd42ae4ee9dc5d3b730297169a0213dfe0eeb86f
adentintime <arixbizz@gmail.com>
2018-07-20 20:04:50 +0000

    Update 0, won't execute on import

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
+"""Lists all states from a database."""
 
 
 import MySQLdb
 from sys import argv
 
 
-def main():
-    """Lists all states from a database."""
-    username = argv[1]
-    password = argv[2]
-    database = argv[3]
+username = argv[1]
+password = argv[2]
+database = argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                            passwd=password, db=database)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states".format(database))
-    rows = cur.fetchall()
-    for x in rows:
-        print(x)
-
-if __name__ == "___main__":
-    main()
+db = MySQLdb.connect(host="localhost", port=3306, user=username,
+						passwd=password, db=database)
+cur = db.cursor()
+cur.execute("SELECT * FROM states".format(database))
+rows = cur.fetchall()
+for x in rows:
+	print(x)
