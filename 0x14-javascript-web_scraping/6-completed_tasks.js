e61287d6ad99186743eb64c14c6696c5c4a23eaa
adentintime <arixbizz@gmail.com>
2018-09-06 21:18:28 +0000

    Create 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+let count = 0;
+let dict = {};
+
+request.get(url, (error, response, body) => {
+  if (error) console.log(error);
+  else {
+    for (const key in JSON.parse(body)) {
+      let uid = JSON.parse(body)[key]['userId'];
+      if (uid && JSON.parse(body)[key]['completed'] == true) {
+        if (dict[uid] == undefined) {
+          dict[uid] = 1;
+        }
+        else {
+          dict[uid] += 1;
+        }
+      }
+    }
+  }
+  console.log(dict);
+});
