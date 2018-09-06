6887c311c7d272a51e28e321f92d042ac1f9e9c2
adentintime <arixbizz@gmail.com>
2018-09-06 20:29:13 +0000

    Create 3-4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+const WedgeAntilles = '18';
+const characters = '?characters=';
+const url = process.argv[2] + characters + WedgeAntilles;
+let count = 0;
+
+request.get(url, (error, response, body) => {
+  if (error) console.log(error);
+  else {
+    for (const key in JSON.parse(body).results) {
+      for (const key2 in JSON.parse(body).results[key]) {
+        if (key2 === 'characters') {
+          for (const key3 in JSON.parse(body).results[key][key2]) {
+            if (JSON.parse(body).results[key][key2][key3] === 'https://swapi.co/api/people/18/') {
+              count++;
+              // console.log(JSON.parse(body).results[key][key2][key3]);
+            }
+          }
+        }
+      }
+    }
+  }
+  console.log(count);
+});
